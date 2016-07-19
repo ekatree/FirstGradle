a5b14cbddeae13b8ab4263946ee2d571768b18dc
java <java>
2016-07-19 16:25:06 +0900

    10ea Hello Lable change

--- src/main/java/com/hybrid/MainApplication.java
+++ src/main/java/com/hybrid/MainApplication.java
@@ -4,9 +4,11 @@ public class MainApplication{
 
 	public static void main(String[] args){
 
-		System.out.println("####################");
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@");
+		for(int i=0; i<10; i++){
 		System.out.println("Hello First Gradle !");
-		System.out.println("####################");	
+		}
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@");	
 
 	}
 
